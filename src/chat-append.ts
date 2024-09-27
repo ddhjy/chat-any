2d6b54b3292eedc893d78996c5ef9794cf6db661
曾凯 <zengkai@bytedance.com>
2024-09-27 13:55:27 +0800

    Clean up comments in chat and chat-append modules

--- src/chat-append.ts
+++ src/chat-append.ts
@@ -1,7 +1,5 @@
-// src/chat-append.ts
 import { handleChatOperation } from "./common";
 
-// 主函数
 export default async function Command() {
   await handleChatOperation('append');
 }
--- src/chat.ts
+++ src/chat.ts
@@ -1,7 +1,5 @@
-// src/chat.ts
 import { handleChatOperation } from "./common";
 
-// 主函数
 export default async function Command() {
   await handleChatOperation('write');
 }
